5a0e6b410871dae3d0b9f881a86843fc91bd44b7
Victor Escalona <victorescalonag@gmail.com>
2021-12-03 17:35:00 -0500

    chore: Added i18n I18Next package
    Added i18n to handle multiple languages inside the application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
+
 //Tailwinds
 import "./styles/output.css";
 
+//i18n
+import { useTranslation } from "react-i18next";
+
 const App = () => {
+  const { t, i18n } = useTranslation();
+
+  const handleLanguage = (e) => {
+    i18n.changeLanguage(e.target.value);
+  };
   return (
-    <div>
-      <h1 className="text-red-700">React App - Serempres</h1>
-    </div>
+    <Suspense fallback="loading">
+      <h1 className="text-red-400">React App - Serempres</h1>
+      <p>{t("Welcome to React")}</p>
+
+      <select onChange={handleLanguage}>
+        <option value="en">English</option>
+        <option value="es">Español</option>
+      </select>
+    </Suspense>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+//I18NEXT
+import "./i18n";
+
 ReactDOM.render(<App />, document.querySelector("#root"));
